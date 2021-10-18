43a43727bea85f5931c2a1d957086396dfbda706
kilokid <arlomov007@gmail.com>
2021-10-18 21:16:21 +0300

    init project

---
+++ script/script.js
@@ -0,0 +1,37 @@
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+
+  // Timer
+  function countTimer(deadline) {
+    const timerHours = document.querySelector("#timer-hours"),
+      timerMinutes = document.querySelector("#timer-minutes"),
+      timerSeconds = document.querySelector("#timer-seconds");
+
+    function getTimeRemaining() {
+      const dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+				return {timeRemaining, hours, minutes, seconds};
+    }
+
+		function updateClock() {
+			const timer = getTimeRemaining();
+
+			timerHours.textContent = timer.hours;
+			timerMinutes.textContent = timer.minutes;
+			timerSeconds.textContent = timer.seconds;
+
+			if (timer.timeRemaining > 0) {
+				setTimeout(updateClock, 1000);
+			}
+		}
+
+		updateClock();
+  }
+
+	countTimer('20 October 2021');
+	
+});
