bc2e8c14eb7002fd6c2637c5d07086bcbe29751e
kilokid <arlomov007@gmail.com>
2021-10-19 20:34:37 +0300

    finish hard homework

--- script/script.js
+++ script/script.js
@@ -103,4 +103,22 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   togglePopUp();
+
+  // smooth scrool
+  const smoothScrool = () => {
+    const smothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+
+    smothScrollElems.forEach(link => {
+      link.addEventListener('click', (event) => {
+          event.preventDefault();
+          const id = link.getAttribute('href').slice(1);
+          
+          document.getElementById(id).scrollIntoView({
+              behavior: 'smooth',
+          });
+      });
+    });
+  };
+
+  smoothScrool();
 });
