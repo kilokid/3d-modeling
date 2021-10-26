fe7d439c4d1bbd172664b7913aaa1139e82c7724
kilokid <arlomov007@gmail.com>
2021-10-26 15:05:04 +0300

    feat: add calculator functional

--- script/script.js
+++ script/script.js
@@ -42,7 +42,7 @@ window.addEventListener("DOMContentLoaded", () => {
     timerInterval = setInterval(updateTimer, 1000);
   }
 
-  countTimer("25 October 2021");
+  countTimer("27 October 2021");
 
   // menu
   const toggleMenu = () => {
@@ -346,4 +346,48 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   calculatorValidation();
+
+  // calculator functional
+  const calculator = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block');
+    const calcType = document.querySelector('.calc-type');
+    const calcSquare = document.querySelector('.calc-square');
+    const calcDay = document.querySelector('.calc-day');
+    const calcCount = document.querySelector('.calc-count');
+    const totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0;
+      let countValue = 1;
+      let dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value;
+      const squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+      
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
+  };
+
+  calculator(100);
 });
